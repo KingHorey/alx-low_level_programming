0619f47173a8bcd45ecb8c798e2cb74f06166fac
KingHorey <oreoluwaakinbo.oa@gmail.com>
2022-10-13 21:49:51 +0100

    print out the size (in bytes) of data types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - start
+* description - prints out the size in bytes
+* Return: 0 if successful
+*/
+int main(void)
+{
+	printf("size of an integer is: %lu bytes\n", sizeof(int));
+	printf("size of a char is: %lu bytes\n", sizeof(char));
+	printf("size of a long int: %lu  bytes\n", sizeof(long int));
+	printf("size of a long long int: %lu bytes\n", sizeof(long long int));
+	printf("size of a float is: %lu bytes\n", sizeof(float));
+	return (0);
+}
+
