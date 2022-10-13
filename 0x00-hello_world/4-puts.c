b120ad97a02ecabde34369607fea344484bc14b5
KingHorey <oreoluwaakinbo.oa@gmail.com>
2022-10-13 18:01:39 +0100

    making use of puts to output string

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
-
-int main(void)
-{
-	/*
+	/**
 	*function makes use of
 	*the puts option to display a string
 	*/
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
