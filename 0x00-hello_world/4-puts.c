e9fd6a59584ba33ac33081268a5d417164289f1a
KingHorey <oreoluwaakinbo.oa@gmail.com>
2022-10-13 21:19:52 +0100

    making use of puts to output string

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
-	/**
-	* main - Entry point
-	*
-	* Return: Always 0 (success)
-	*/
+/**
+* main - start function
+* description - "\"Programming is like building a multilingual puzzle"
+* Return: 0
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
