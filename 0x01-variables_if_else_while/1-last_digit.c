e4c5109a0d5cb62bc5c169e496a4db0e5cebbcf2
KingHorey <oreoluwaakinbo.oa@gmail.com>
2022-10-14 17:50:10 +0100

    find the last digit of a number n and run  relation operator

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - generates a pseudo random number for the
+ * variable n
+ *
+ * Description: and runs a relational checck to confirm
+ * if the final digit is greater, lesser or equal
+ * to parameters set
+ * The last digit of the int n is gotten by dividing n by 10
+ * using the modulus operator
+ *
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	int n, last_digit;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	last_digit = n % 10;
+	if (last_digit > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5", n, last_digit);
+	} else if (last_digit == 0)
+	{
+		printf("Last digit of %d is %d and is 0", n, last_digit);
+	} else if ((last_digit < 6) && (last_digit != 0))
+	{
+		printf("Last digit of %d is %d is less than 6 and not 0", n, last_digit);
+	}
+	return (0);
+}
+
