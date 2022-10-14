e5ea4045935d60c62ccd41b668ea53912e262ccd
KingHorey <oreoluwaakinbo.oa@gmail.com>
2022-10-14 19:49:24 +0100

    Make use of putchar to print out the alphabet in small letters

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabets in small letters
+ * Description -  This function makes use of the putchar method
+ * to print out the small letters.
+ * Please note that the integers are in ASCII format
+ *
+ * Return: 0 if successful
+ */
+
+int main(void)
+{
+	int n = 97;
+
+while (n <= 122)
+	{
+	putchar(n);
+	n++;
+	}
+	return (0);
+}
