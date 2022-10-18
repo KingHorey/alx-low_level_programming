a77e687b2e206815e316f7114b32d8839b3bd1c9
KingHorey <oreoluwaakinbo.oa@gmail.com>
2022-10-18 19:54:39 +0100

    using the main.h to printout 'putchar'

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <main.h>
+/**
+* main - start point for source code
+* Description: send _putchar to stdout without using
+* printf and puts
+* Return: 0 if successful
+*/
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n);
+	return (0);
+}
