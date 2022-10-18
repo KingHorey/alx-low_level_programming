e17db24c42d330a5e393c26d662ac905bc88886e
KingHorey <oreoluwaakinbo.oa@gmail.com>
2022-10-18 21:25:18 +0100

    check for lowercases using islower()

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _islower - checks if a character is lowercase
+ * returns 0 if it is lowercase and 1 if it is uppercase
+ * @c: character to be checked
+ *
+ */
+int _islower(int c)
+{
+	int checker;
+
+	checker = islower(c);
+	_putchar(checker);
+}
+
