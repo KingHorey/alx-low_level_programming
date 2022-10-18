1292343f91fb47dd2c332e2d2be37f6c9c644de8
KingHorey <oreoluwaakinbo.oa@gmail.com>
2022-10-18 21:57:20 +0100

    check for lowercases using _islower()

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,14 +3,20 @@
  * _islower - checks if a character is lowercase
  * returns 0 if it is lowercase and 1 if it is uppercase
  * @c: character to be checked
- *
+ * Return: 0 if successful
  */
+
 int _islower(int c)
 {
-	int checker;
-
-	checker = _islower(c);
-	_putchar(checker);
+	char checker[] = {'H', 'o', 'O', 'm'};
+	int i;
+	char checkers;
+	
+	for(i=0; checker[i];i++)
+	{
+	checkers = _islower(checker[i]);
+	_putchar(checkers);
+	}
 	return (0);
 }
 
