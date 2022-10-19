b06e8262e2996c038a577bb0e038c730d9cada2f
KingHorey <oreoluwaakinbo.oa@gmail.com>
2022-10-19 19:13:29 +0100

    function prints out the time in 24 hours format

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * jack_bauer- function prints the time in 24hours
+ */
+
+void jack_bauer(void)
+{
+	unsigned short int hh;
+	unsigned short int mm;
+
+	for (hh = 00 ; hh < 24; hh++)
+	{
+		for (mm = 00; mm < 60; mm++)
+		{
+			if ((hh <= 10) && (mm < 10))
+			{
+				printf("%02d:%02d\n", hh, mm);
+			}
+			else if ((hh <= 10) && (mm >= 10))
+			{
+				printf("%02d:%02d\n", hh, mm);
+			}
+			else if ((hh > 10) && (mm < 10))
+			{
+				printf("%d:%02d\n", hh, mm);
+			}
+			else
+			{
+				printf("%d:%d\n", hh, mm);
+			}
+		}
+	}
+}
+
