588492bdab312855ef4df0f1f3fae04e74009b6b
KingHorey <oreoluwaakinbo.oa@gmail.com>
2022-10-19 09:58:41 +0100

    Checks for alphabets using  _isalpha()

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _isalpha - prototypes function to test for alphabets
+ * @c: argument to be checked
+ * Description : Function checks the parameter to see if the provided
+ * parameter is an alphabet
+ *
+ * Return: 1 if a n alphabet, 0 if not.
+ *
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 65) && (c <= 90))
+	|| ((c >= 97) && (c <= 12))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
+
