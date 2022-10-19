f7ba606b0dc2d872ebc43ccae73a6fac5e8229ee
KingHorey <oreoluwaakinbo.oa@gmail.com>
2022-10-19 13:06:19 +0100

    Print out absolute value of any integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _abs-function checks for absolute value
+ * @int: value to be checked
+ * Return: positive value of int
+ */
+
+int _abs(int)
+{
+	int c;
+
+	c = _abs(int);
+	_putchar(c);
+	_putchar('\n');
+}
+
