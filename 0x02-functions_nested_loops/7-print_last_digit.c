5689e70231c4adc05c87fe34f3ccf573d4ef132e
KingHorey <oreoluwaakinbo.oa@gmail.com>
2022-10-19 14:06:18 +0100

    function to find the last digit of any given integer

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_last_digit- prints the last digit of any given integer
+ * @c: argument to be checked
+ * Return: the last digit
+ */
+
+int print_last_digit(int c)
+{
+	int last_digit
+
+	last_digit = c % 10 ;
+	return (last_digit);
+}
+
