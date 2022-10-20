89bd100ec89fe1a142e2118ecae223ee57dcb842
KingHorey <oreoluwaakinbo.oa@gmail.com>
2022-10-20 14:42:03 +0100

    print numbers from 0 to 9 then add a new line (10 times)

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * more_numbers- prints numbers
+ *
+ * Description: Function prints numbers from 1 to 14
+ * followed by a new line (10 times)
+ * Return: 0
+ */
+void more_numbers(void)
+{
+	int numbers, counter = '0';
+
+	for (counter = '0'; counter < '10'; counter++)
+	{
+		for (number = '0'; numbers < '15'; numbers++)
+		{
+			_putchar(number);
+		}
+	_putchar(10);
+	}
+}
+
+
