fa34153157cd26c25e0a6d92c633c8c8609c8a22
KingHorey <oreoluwaakinbo.oa@gmail.com>
2022-10-20 10:41:58 +0100

    checks for single digit numbers

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,21 @@
+include"main.h"
+/**
+ * _isdigit- checks the argument for numbers
+ * @c: argument to be evaluated
+ * Return: 1 if argument is a digit
+ * 0 if otherwise
+ *
+ */
+
+int _isdigit(int c)
+{
+	if ((c >= '0') && (c <= 9))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
+
