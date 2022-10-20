ac73e13527334a49b2d315fb6d7a2914170f85a5
KingHorey <oreoluwaakinbo.oa@gmail.com>
2022-10-20 16:03:09 +0100

    outputs '\' by the number of times specified by the user

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ *
+ *
+ */
+void print_diagonal(int n)
+{
+	
+	int x;
+
+	for (x = 0; x < n; x++)
+	{
+		if (n <= 0)
+		{
+			putchar(10);
+		}
+		else
+		{
+			putchar(92);
+		}
+	}
+	putchar(10);
+}
+
