622a6e4f1b64db71c1f477635b25222647a521de
KingHorey <oreoluwaakinbo.oa@gmail.com>
2022-10-20 15:45:32 +0100

    outputs '_' by the number of times specified by the user

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_line- outputs '_' by the number of times specified by the
+ * user
+ * @n: the number of times '_' is to be outputted
+ * Return: 0
+ */
+void print_line(int n)
+{
+	unsigned short int x;
+
+	for (x = 0; x < n; x++)
+	{
+		if (n <= 0)
+		{
+			_putchar(10);
+		}
+		else
+		{
+			_putchar(45);
+		}
+	}
+	_putchar(10);
+	return (0);
+}
