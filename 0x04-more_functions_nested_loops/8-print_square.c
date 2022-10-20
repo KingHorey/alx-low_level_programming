4e7704c2d8b9198583a67332340336d5bbdf5611
KingHorey <oreoluwaakinbo.oa@gmail.com>
2022-10-20 17:36:43 +0100

    outputs # through row and columns by the number of times specified

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * print_square - prints '#' the number of times specified by the user
+ * both row and column
+ *
+ * @size- number of times '#' is to be printed
+ */
+
+void print_square(int size)
+{
+	int outer, inner;
+
+	if (size <= 0)
+	{
+		putchar(10);
+	}
+	else
+	{
+		for (outer = 0; outer < size; outer++)
+		{
+			_putchar('#');
+			for (inner = 1; inner < n; inner++)
+			{
+				_putchar('#');
+			}
+			_putchar(10);
+		}
+	}
+}
+
