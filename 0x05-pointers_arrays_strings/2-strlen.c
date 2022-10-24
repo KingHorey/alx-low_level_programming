d9c00b8703a9f6db22c2b05b0f6bc2a16afcba4d
KingHorey <oreoluwaakinbo.oa@gmail.com>
2022-10-24 21:00:55 +0100

    finding the length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+/**
+ * _strlen- calculates the length of any provided string
+ * @s: string to be checked
+ * Return: 0
+ */
+
+int _strlen(char *s)
+{
+	int length;
+
+	length = _strlen(s);
+	_putchar((length / 10) + '0');
+	_putchar((length % 10) + '0');
+	return (0);
+}
