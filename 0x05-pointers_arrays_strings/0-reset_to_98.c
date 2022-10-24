5e7c61ff1814ca4ec3e299cf88a78dc84907b7f6
KingHorey <oreoluwaakinbo.oa@gmail.com>
2022-10-24 14:50:58 +0100

    fixed multiple _putchar error

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,10 @@
-#include "main.h"
+/*#include "main.h"*/
 /**
  * reset_to_98- function changes the value of an argument to 98
  * @n: argument to be changed
  */
 
-void reset_to_98(int *n);
+void reset_to_98(int *n)
 {
 	*n = 98;
-	_putchar(10);
 }
