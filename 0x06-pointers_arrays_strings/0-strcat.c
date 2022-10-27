0cdd05ab49f55c2f7a4abdc4c47f64bc188e793f
KingHorey <oreoluwaakinbo.oa@gmail.com>
2022-10-27 14:08:46 +0100

    fixed betty documentation errors

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,13 +2,10 @@
 
 /**
  * _strcat- concatenates two strings into the dest array/pointer
- *
  *@dest: pointer that other strings will be added to
  *@src: pointer/array to be copied
- * Description: This function concatenates two srings into one
  *Return: concatenated array
  */
-int _strlen(char *s);
 
 char *_strcat(char *dest, char *src)
 {
@@ -20,16 +17,18 @@ char *_strcat(char *dest, char *src)
 
 	while (src[i] != '\0')
 	{
-		dest[size] = src[i];
+		*(dest + size) = *(src + i);
 		size++;
 		i++;
 	}
 	return (dest);
 }
+
 /**
  *_strlen- Calculates length of the destination array
- *@s: Array to get length
+ *@s : Array to get length
  *Return: length of the array
+ *
  */
 
 int _strlen(char *s)
