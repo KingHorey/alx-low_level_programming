5e80c46076d10f952a95ca6b1360aec6fc69e7be
KingHorey <oreoluwaakinbo.oa@gmail.com>
2022-10-30 05:53:48 +0100

    copies a string by n number of bytes

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,49 +1,15 @@
 #include "main.h"
 
 /**
- * _strncpy- copied a string
- * @dest: the destination array
- * @src: array to be copied
- * @n: number of bytes to be used when copying
- * Time: 19:52
- * date: 27th Oct 22
- * Author: Oreoluwa S. Akinbo
- * Return: The new array with the copied content
- */
-
+* _strncpy -> a copying function
+* @dest: parameter 1
+* @src: parameter 2
+* @n: parameter 3
+* Return: string
+*/
 char *_strncpy(char *dest, char *src, int n)
 {
-	int length = _strlen(dest);
-
-	int i;
-
-	for (i = 0; i < length; i++)
-	{
-		if (i < n)
-		{
-			dest[i] = src[i];
-		}
-		else if (i == n)
-			break;
-	}
-
+	strncpy(dest, src, n);
 	return (dest);
+
 }
-
-/**
- *_strlen- calculates the length of a string
- * @s: string to be calculated
- * Return: the length of the string
- */
-
-int _strlen(char *s)
-{
-	int length = 0;
-
-	while (s[length] != '\0')
-	{
-		length++;
-	}
-	return (length);
-}
-
