996715a83f65c42bbd23f9999f866329b0ceb60f
KingHorey <oreoluwaakinbo.oa@gmail.com>
2022-10-31 13:32:14 +0100

    made adjustments for betty compliance

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -2,15 +2,17 @@
 
 /**
  *_memset - replaces a part of an array with a character
+ * @s: the destination array
  * @b: the character to be inserted
  * @n: number of times to be replaced
+ * Return: newly array with copied content
  */
 
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int count;
-	
-	for(count = 0; count < n; count++)
+
+	for (count = 0; count < n; count++)
 	{
 		*(s + count) = b;
 	}
