6f584327d0e8d6cad18dd717ccec848268433e22
KingHorey <oreoluwaakinbo.oa@gmail.com>
2022-11-01 10:21:44 +0100

    locate the character ins a string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strchr- print occurence of a char
+ * @s: arrayto be scanned
+ * @c: character to look for
+ * Return: the array
+ */
+
+char *_strchr(char *s, char c)
+{
+	unsigned short int index, inner;
+
+	for (index = 0; s[index] != '\0'; index++)
+	{
+		if (*(s + index) == c)
+		{	
+			for (inner = index; s[inner] != '\0'; inner++)
+			{
+				_putchar(*(s + inner));
+			}
+			break;
+		}
+	}
+	
+
+}
+
