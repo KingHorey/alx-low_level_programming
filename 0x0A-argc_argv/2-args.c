aafecaf6cb0deb026a798454703a5fc3617e4174
KingHorey <oreoluwaakinbo.oa@gmail.com>
2022-11-05 12:06:06 +0100

    prints each argument followed by a new line

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main- program start and end point
+ * Description: prints all arguments provided
+ * @argc: argument count
+ * @argv: pointer to an array pointer
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+ 	int i = 0;
+
+	while (i < argc)
+	{
+		printf("%s\n", argv[i]);
+		i++;
+	}
+	return (0);
+}
