44ee0973fd9ea131f0d08c2e2dab324a378273f9
KingHorey <oreoluwaakinbo.oa@gmail.com>
2022-11-05 11:46:27 +0100

    Added __attribute__ for argument *argv

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - start point of program
+ * @argc: argument count
+ * @argv: array containing all arguments
+ * @Return: 0 if successful
+ *
+ */
+
+int main(int argc, char __attribute__((unused)) *argv[])
+{
+	printf("%d", argc);
+
+	return (0);
+
+}
+
