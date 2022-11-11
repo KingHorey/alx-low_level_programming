05abff9bdb97bc8a7f3452fc92286a1a672eac39
KingHorey <oreoluwaakinbo.oa@gmail.com>
2022-11-11 20:59:16 +0100

    fixed while loop conditional

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,64 +1,46 @@
 #include "main.h"
 #include <stdlib.h>
 
-/**
- * string_nconcat- function concatenates two strings into one
- * by a number of bytes
- * @s1: destination pointer
- * @s2: pointer whose strings are to be concatenated
- * @n: number of bytes to concatenate by
- * Return: newly allocated memory
- */
-
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *new_str;
+	unsigned int i = 0, len1 = 0, len2 = 0, i2 = 0;
+	char *str;
 
-	int len = _strlen(s1) + 1; /* get the length of s1 */
-
-	int index1 = 0;
-	unsigned int index2 = 0;
-
-	new_str = malloc(len * n);
-
-	if (new_str == NULL)
+	while (s1 && s1[len1])
 	{
+		len1++;
+	}
+
+	while (s2 && s2[len2])
+	{
+		len2++;
+	}
+
+	if (n < len2)
+		str = malloc(sizeof(char) * (len1 + n + 1));
+	else
+		str = malloc(sizeof(char) * (len1 + len2 + 1));
+
+	
+	if (str == NULL)
 		return (NULL);
-	}
 
-	while (s1[index1] != '\0')
+	while (i < len1)
 	{
-		new_str[index1] = s1[index1];
-		index1++;
+		str[i] = s1[i];
+		i++;
 	}
 
-	while (index2 < n)
-	{
-		new_str[index1] = s2[index2];
-		index2++;
-		index1++;
-	}
+	while (n < len2 && i < (len1 + n))
+		str[i++] = s2[i2++];
+	while (n >= len2 && i < (len1 + len2))
+		str[i++] = s2[i2++];
 
-	return (new_str);
+	str[i] = '\0';
+
+
+	return (str);
 
 }
 
-/**
- * _strlen- calculates the length of a string
- * @c: string to be calculated
- * Return: length of the string
- */
 
-int _strlen(char *c)
-{
-	int length = 0;
-	int index = 0;
-
-	while (c[index] != '\0')
-	{
-		index++;
-		length++;
-	}
-
-	return (length);
-}
