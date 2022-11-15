e1891ed7aeabb80cceb93656c384d58137d2eaea
KingHorey <oreoluwaakinbo.oa@gmail.com>
2022-11-15 01:55:28 +0100

    updated header file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+*struct dog - A structure data type for dog information
+*@name: Dog's name
+*@age: Dog's age
+*@owner: Dog owner's name
+*/
+
 typedef struct dog 
 {
 	char *name;
