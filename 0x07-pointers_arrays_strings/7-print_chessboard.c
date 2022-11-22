7b1230a0adbe91ec62325e89dcfb1ee009ef4720
KingHorey <oreoluwaakinbo.oa@gmail.com>
2022-11-22 03:04:12 +0100

    optimised code and used for loop

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,5 +1,3 @@
-
-
 #include "main.h"
 
 /**
@@ -9,20 +7,16 @@
  */
 void print_chessboard(char (*a)[8])
 {
-	/*Declaring variables*/
 	int i, j;
 
 	i = 0;
-	while (i < 8) /*number repetitions*/
+	for(i = 0; i < 8; i++)
 	{
-		j = 0;
-		while (j < 8)
+		for (j = 0; j < 8; j++)
 		{
 			_putchar (a[i][j]);
-			j++;
 		}
-		_putchar ('\n'); /*print new line*/
-		i++; /*add +1*/
+		_putchar (10);
 	}
 
 }
