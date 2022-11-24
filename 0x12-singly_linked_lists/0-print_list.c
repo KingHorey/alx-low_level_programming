78dca6d1df700a48e5b68d55c0516ff0bab594e4
KingHorey <oreoluwaakinbo.oa@gmail.com>
2022-11-24 21:30:58 +0100

    singly linked task

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * print_list - prints all data in a list
+ * @h: pointer to first node
+ * Return: number of nodes
+*/
+size_t print_list(const list_t *h)
+{
+		char *s;
+	size_t i = 0;
+	int j, k;
+
+	while (h != NULL)
+	{
+		s = h->str;
+		k = h->len;
+
+		if (s == NULL)
+		{
+			printf("[%d] (nil)", 0);
+		}
+		else
+		{
+			printf("[%d] ", k);
+			j = 0;
+
+			while (s[j] != '\0')
+			{
+				printf("%c", s[j]);
+				j++;
+			}
+		}
+		i++;
+		printf("\n");
+		h = h->next;
+	}
+	return (i);
+}
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,8 @@
+* 0-print_list.c - prints all the element of a list_t list
+* 1-list_len.c - returns the number of elements in a linked list
+* 2-add_node.c - adds a new node at the beginning of a list_t list
+* 3-add_node_end.c - adds a new node at the end of a list_t list
+* 4-free_list.c - frees a list_t list
+* 100-first.c - prints a string before the main function is executed
+* 101-hello_holberton.asm - program is a 64-bit assembly program that prints "Hello, Holberton" followed by a new line
+
