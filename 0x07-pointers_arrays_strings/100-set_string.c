69b02d38a4d8618836c979dc9c585a5d27b77bce
KingHorey <oreoluwaakinbo.oa@gmail.com>
2022-11-24 04:19:06 +0100

    changes the vaue of a pointer to a pointer

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * set_string- changes the value of the destination pointer
+ * @s: pointer to be altered
+ * @to: source pointer
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
