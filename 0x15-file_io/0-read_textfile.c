646b530802bafe5d41c10ec935c060bd28ae0147
KingHorey <oreoluwaakinbo.oa@gmail.com>
2022-12-05 19:12:45 +0100

    open file and write to POSIX output

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads the content of a file and prints to stdout
+ *
+ * @filename: file to be opened
+ * @letters: number of char to read
+ *
+ * Return: number of char printed
+ */
+
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd, i = 0;
+	char *buf;
+	buf = malloc(sizeof(char) * letters);
+
+	if (filename == NULL)
+		return (0);
+
+
+	fd = open(filename, O_RDWR, 00777); /* open txt file */
+
+	if (fd == -1)
+	{
+		return (0);
+	}
+
+	read(fd, buf, letters); /*read content and save to array */
+	buf[letters + 1] = '\0';
+
+
+	close(fd);
+
+	while (buf[i])
+	{
+		_putchar(buf[i++]);
+	}
+
+	return (letters);
+
+}
