8ab7f9ab419a4e974b5e0ede7c48fbf4dc3a9370
KingHorey <oreoluwaakinbo.oa@gmail.com>
2022-12-06 01:14:24 +0100

    made few changes for when no content is passed

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,7 +13,7 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int fd, r_w;
+	int fd, r_w, len;
 	char *buf;
 
 	if (filename == NULL)
@@ -22,18 +22,26 @@ int create_file(const char *filename, char *text_content)
 	if (text_content == NULL)
 	{
 		fd = open(filename, O_CREAT, 0600);
+		len = 0;
 	}
 	else
+	{
 		fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 
-	if (fd == -1)
-		return (-1);
+		len = _strlen(text_content) + 1;
 
-	buf =  malloc(sizeof(char) * _strlen(text_content));
+		if (fd == -1)
+			return (-1);
 
-	r_w = write(fd, text_content, _strlen(text_content) + 1);
+		buf =  malloc(sizeof(char) * len);
+	}
+
+	r_w = write(fd, text_content, len);
+
 	if (r_w == -1)
 		return (-1);
+
+	buf[len + 1] = '\0';
 
 	close(fd);
 	free(buf);
