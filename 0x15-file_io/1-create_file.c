fff951a096a4b87bc1d2e1b4d4ebcbbfabc557df
KingHorey <oreoluwaakinbo.oa@gmail.com>
2022-12-06 01:34:09 +0100

    removed malloc

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,7 +14,7 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fd, r_w, len;
-	char *buf;
+	/* char *buf; */
 
 	if (filename == NULL)
 		return (-1);
@@ -28,7 +28,7 @@ int create_file(const char *filename, char *text_content)
 		len = _strlen(text_content) + 1;
 	}
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	buf = malloc(sizeof(char) * len);
+	/* buf = malloc(sizeof(char) * len);*/
 
 	if (fd == -1)
 		return (-1);
@@ -38,10 +38,10 @@ int create_file(const char *filename, char *text_content)
 	if (r_w == -1)
 		return (-1);
 
-	buf[len + 1] = '\0';
+	/* buf[len + 1] = '\0'; */
 
 	close(fd);
-	free(buf);
+	/*free(buf); */
 
 	return (1);
 
