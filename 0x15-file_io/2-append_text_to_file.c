94a724f0206a52e034c6c1e0c1cc5038adff887a
KingHorey <oreoluwaakinbo.oa@gmail.com>
2022-12-08 21:15:34 +0100

    fixed outpur count error

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -20,7 +20,7 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (file == -1)
 		return (-1);
-	len = _strlen(text_content) + 1;
+	len = _strlen(text_content);
 	r_write = write(file, text_content, len);
 
 	if (r_write == -1)
