f5b91f74f3674d0a1669c29db93b34bcb5e026b4
KingHorey <oreoluwaakinbo.oa@gmail.com>
2022-12-08 21:14:05 +0100

    fixed output count error

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -29,7 +29,7 @@ int create_file(const char *filename, char *text_content)
 	}
 	else
 	{
-		len = _strlen(text_content) + 1;
+		len = _strlen(text_content);
 	}
 
 	r_w = write(fd, text_content, len);
