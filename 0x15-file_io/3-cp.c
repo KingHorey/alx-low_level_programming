136f5a837e1f0a45f9f4a61ccd7b0eede41a46a6
KingHorey <oreoluwaakinbo.oa@gmail.com>
2022-12-10 20:32:34 +0100

    fixed few other errors

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,6 +1,6 @@
 #include "main.h"
 #define SIZE 1024
-#define STDERR_FILENO 2
+#define ERR STDERR_FILENO
 
 /**
  * open_file - copies content from a file a to a file b
@@ -16,19 +16,19 @@ void open_file(const char *src_file, const char *dst_file)
 	fd_src = open(src_file, O_RDONLY);
 	if (fd_src == -1)
 	{
-		dprintf(2, "Error: Can't read from file %s\n", src_file);
+		dprintf(ERR, "Error: Can't read from file %s\n", src_file);
 		exit(97);
 	}
 	fd_one_r = read(fd_src, buf, SIZE);
 	if (fd_one_r == -1)
 	{
-		dprintf(2, "Error: Can't read from file %s\n", src_file);
+		dprintf(ERR, "Error: Can't read from file %s\n", src_file);
 		exit(98);
 	}
 	fd_dst = open(dst_file, O_CREAT | O_RDWR | O_TRUNC, 00664);
 	if (fd_dst == -1)
 	{
-		dprintf(2, "Error: can't write to %s\n", dst_file);
+		dprintf(ERR, "Error: can't write to %s\n", dst_file);
 		exit(99);
 	}
 
@@ -37,17 +37,17 @@ void open_file(const char *src_file, const char *dst_file)
 		dst_write = write(fd_dst, buf, fd_one_r);
 		if (dst_write == -1)
 		{
-			dprintf(2, "Error: can't write to %s", dst_file);
+			dprintf(ERR, "Error: can't write to %s\n", dst_file);
 			exit(99);
 		}
 		fd_one_r = read(fd_src, buf, SIZE);
 	}
 	close_input1 = close(fd_src);
 	if (close_input1 == -1)
-		dprintf(2, "Error: Can't close fd %d\n", fd_src), exit(100);
+		dprintf(ERR, "Error: Can't close fd %d\n", fd_src), exit(100);
 	close_input2 = close(fd_dst);
 	if (close_input2 == -1)
-		dprintf(2, "Error: Can't close fd %d\n", fd_dst), exit(100);
+		dprintf(ERR, "Error: Can't close fd %d\n", fd_dst), exit(100);
 	free(buf);
 }
 
