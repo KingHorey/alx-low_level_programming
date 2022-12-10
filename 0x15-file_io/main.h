bea337b8b658dc2e1ad79782520f3cd141471c69
KingHorey <oreoluwaakinbo.oa@gmail.com>
2022-12-10 05:30:34 +0100

    added copy function

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -6,6 +6,7 @@
 #include <fcntl.h>
 #include <stddef.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 #include <unistd.h>
 
@@ -14,5 +15,6 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *, char *);
 int append_text_to_file(const char *filename, char *text_content);
 int _strlen(char *);
+void open_file(const char *, const char *);
 
 #endif
