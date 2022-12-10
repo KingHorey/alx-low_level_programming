73892c3d89edd5d86f0159e607263eeae332ddb5
KingHorey <oreoluwaakinbo.oa@gmail.com>
2022-12-10 20:15:08 +0100

    fixed a few errors

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -9,17 +9,17 @@
 void open_file(const char *src_file, const char *dst_file)
 {
 	char *buf;
-	int fd_one, fd_two, fd_one_r, close_input1, close_input2, dst_write;
+	int fd_src, fd_dst, fd_one_r, close_input1, close_input2, dst_write;
 
 	buf = malloc(sizeof(char) * SIZE);
-	fd_one = open(src_file, O_RDONLY);
-	if (fd_one == -1)
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s", src_file),exit(97);
-	fd_one_r = read(fd_one, buf, SIZE);
+	fd_src = open(src_file, O_RDONLY);
+	if (fd_src == -1)
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", src_file), exit(97);
+	fd_one_r = read(fd_src, buf, SIZE);
 	if (fd_one_r == -1)
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s", src_file), exit(98);
-	fd_two = open(dst_file, O_CREAT | O_RDWR | O_TRUNC, 00664);
-	if (fd_two == -1)
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", src_file), exit(98);
+	fd_dst = open(dst_file, O_CREAT | O_RDWR | O_TRUNC, 00664);
+	if (fd_dst == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: can't write to %s\n", dst_file);
 		exit(99);
@@ -27,20 +27,20 @@ void open_file(const char *src_file, const char *dst_file)
 
 	while (fd_one_r)
 	{
-		dst_write = write(fd_two, buf, fd_one_r);
+		dst_write = write(fd_dst, buf, fd_one_r);
 		if (dst_write == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: can't write to %s", dst_file);
 			exit(99);
 		}
-		fd_one_r = read(fd_one, buf, SIZE);
+		fd_one_r = read(fd_src, buf, SIZE);
 	}
-	close_input1 = close(fd_one);
+	close_input1 = close(fd_src);
 	if (close_input1 == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_one), exit(100);
-	close_input2 = close(fd_two);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_src), exit(100);
+	close_input2 = close(fd_dst);
 	if (close_input2 == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_two), exit(100);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_dst), exit(100);
 	free(buf);
 }
 
