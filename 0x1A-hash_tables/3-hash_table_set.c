a396f0db77214173a2f231c2d0a4a44e575c4418
KingHorey <oreoluwaakinbo.oa@gmail.com>
2023-02-04 02:52:55 +0100

    changed return value

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -18,26 +18,20 @@ hash_node_t *create_node(const char *key, const char *value)
 	new_node = malloc(sizeof(struct hash_node_s));
 
 	if (!new_node)
-		return (NULL);
+		return (0);
 	new_node->key = malloc(strlen(key) + 1);
 
 	if (!new_node->key)
 	{
 		free(new_node);
-		return (NULL);
+		return (0);
 	}
 	new_node->value = malloc(strlen(value) + 1);
 
 	if (!new_node->value)
 	{
 		free(new_node);
-		return (NULL);
-	}
-	new_node->next = malloc(sizeof(hash_node_t));
-	if (!new_node->next)
-	{
-		free(new_node);
-		return (NULL);
+		return (0);
 	}
 	strcpy(new_node->key, key);
 	strcpy(new_node->value, value);
@@ -63,11 +57,9 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	hash_node_t *current_item = ht->array[index];
 
+
 	if (current_item == NULL)
-	{
-		item->next = ht->array[index];
 		ht->array[index] = item;
-	}
 
 	return (1);
 }
