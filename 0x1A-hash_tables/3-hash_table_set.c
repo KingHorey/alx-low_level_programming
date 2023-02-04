ecbdf232097ef3b89955f6023b24dd041b5c246c
KingHorey <oreoluwaakinbo.oa@gmail.com>
2023-02-04 04:13:04 +0100

    test remove link to pointer

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -40,7 +40,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	}
 	strcpy(new_node->key, key);
 	strcpy(new_node->value, value);
-	new_node->next = ht->array[index];
 	ht->array[index] = new_node;
 
 	return (1);
