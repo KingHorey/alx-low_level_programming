95a023b00535189ff149fa32c061db33c996aceb
KingHorey <oreoluwaakinbo.oa@gmail.com>
2023-03-30 22:52:19 +0100

    island perimeter

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -21,72 +21,75 @@ def island_perimeter(grid):
     perimeter = 0
 
     for a in range(length):
-        for b in range(len(grid[a])):
-            if grid[a][b] == 1:
-                if b == 0:
-                    perimeter += 1
-                    try:
-                        if grid[a + 1][b] == 0:
-                            perimeter += 1
-                    except IndexError:
+        if all(item == 1 for item in grid[a]):
+            perimeter += 1
+        else:
+            for b in range(len(grid[a])):
+                if grid[a][b] == 1:
+                    if b == 0:
                         perimeter += 1
-                        pass
-                    try:
-                        if grid[a][b+1] == 0:
+                        try:
+                            if grid[a + 1][b] == 0:
+                                perimeter += 1
+                        except IndexError:
                             perimeter += 1
-                    except IndexError:
-                        perimeter += 1
-                        pass
-                    try:
-                        if grid[a-1][b] == 0:
-                            perimeter += 1
-                    except IndexError:
-                        perimeter += 1
-                        pass
-                elif b == len(grid[a]) - 1:
-                    perimeter += 1
-                    try:
-                        if grid[a + 1][b] == 0:
-                            perimeter += 1
-                    except IndexError:
-                        perimeter += 1
-                        pass
-                    try:
-                        if grid[a-1][b] == 0:
-                            perimeter += 1
-                    except IndexError:
-                        perimeter += 1
-                        pass
-                    try:
-                        if grid[a][b-1] == 0:
-                            perimeter += 1
-                    except IndexError:
-                        pass
-                else:
-                    try:
-                        if grid[a + 1][b] == 0:
-                            perimeter += 1
-                    except IndexError:
-                        perimeter += 1
-                        pass
-                    try:
-                        if grid[a][b+1] == 0:
-                            perimeter += 1
-                    except IndexError:
-                        perimeter += 1
-                        pass
-                    try:
-                        if grid[a-1][b] == 0:
-                            perimeter += 1
-                    except IndexError:
-                        perimeter += 1
-                        pass
-                    try:
-                        if b == 0:
                             pass
-                        else:
+                        try:
+                            if grid[a][b+1] == 0:
+                                perimeter += 1
+                        except IndexError:
+                            perimeter += 1
+                            pass
+                        try:
+                            if grid[a-1][b] == 0:
+                                perimeter += 1
+                        except IndexError:
+                            perimeter += 1
+                            pass
+                    elif b == len(grid[a]) - 1:
+                        perimeter += 1
+                        try:
+                            if grid[a + 1][b] == 0:
+                                perimeter += 1
+                        except IndexError:
+                            perimeter += 1
+                            pass
+                        try:
+                            if grid[a-1][b] == 0:
+                                perimeter += 1
+                        except IndexError:
+                            perimeter += 1
+                            pass
+                        try:
                             if grid[a][b-1] == 0:
                                 perimeter += 1
-                    except IndexError:
-                        pass
+                        except IndexError:
+                            pass
+                    else:
+                        try:
+                            if grid[a + 1][b] == 0:
+                                perimeter += 1
+                        except IndexError:
+                            perimeter += 1
+                            pass
+                        try:
+                            if grid[a][b+1] == 0:
+                                perimeter += 1
+                        except IndexError:
+                            perimeter += 1
+                            pass
+                        try:
+                            if grid[a-1][b] == 0:
+                                perimeter += 1
+                        except IndexError:
+                            perimeter += 1
+                            pass
+                        try:
+                            if b == 0:
+                                pass
+                            else:
+                                if grid[a][b-1] == 0:
+                                    perimeter += 1
+                        except IndexError:
+                            pass
     return (perimeter)
