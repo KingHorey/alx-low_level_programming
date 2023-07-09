787e4cfde7c3f977d84d9714a705b0f8bb3cb476
kinghorey <oreoluwaakinbo.oa@gmail.com>
2023-07-09 02:56:44 +0100

    check for null in binary_to_uint function

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -12,16 +12,17 @@
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int result;
-	unsigned int strLen = length(b);
-	unsigned int len = strLen - 1;
-	unsigned int index = len;
+	unsigned int strLen;
+	unsigned int len;
+	unsigned int index;
 	unsigned int i = 0;
 	unsigned int decNum = 0;
 
-	if (!b)
-		return 0;
-	if (strLen == 0)
+	if (!b || !*b)
 		return (0);
+	strLen = length(b);
+	len = strLen - 1;
+	index = len;
 	for (i = 0; i <= len ; i++)
 	{
 		if (*(b + i) < '0' || *(b + i) > '9')
