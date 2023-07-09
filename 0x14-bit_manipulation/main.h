f18f98de83a65c5a0735cf6f8d2d9cc109afe0bb
kinghorey <oreoluwaakinbo.oa@gmail.com>
2023-07-09 02:26:26 +0100

    add header file

---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdlib.h>
+#include <stdio.h>
+
+unsigned int binary_to_uint(const char *b);
+unsigned int power(unsigned int, unsigned int);
+unsigned int length(const char *);
+
+#endif
