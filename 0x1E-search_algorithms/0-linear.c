f2719e9adad23e0086aeed27fa523d60567e523f
kingHorey <oreoluwaakinbo.oa@gmail.com>
2024-03-14 08:27:24 +0100

    change return value to index

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -28,9 +28,7 @@ int linear_search(int *array, size_t size, int value)
 	{
 		printf("Value checked array[%d] = [%d]\n", i, array[i]);
 		if (array[i] == value)
-		{
-			return (array[i]);
-		}
+			return (i);
 		i++;
 	}
 
