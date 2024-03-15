0b1ad8f79f13bfc6d1aa7f450368a14aa3242f2e
kingHorey <oreoluwaakinbo.oa@gmail.com>
2024-03-15 11:18:57 +0100

    fixed recursion and output

--- 0x1E-search_algorithms/0-main.c
+++ 0x1E-search_algorithms/0-main.c
@@ -5,15 +5,20 @@
  *
  * Return: Always EXIT_SUCCESS
  */
+
+#include "search_algos.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-    int array[] = {
-        10, 1, 42, 3, 4, 42, 6, 7, -1, 9
-    };
-    size_t size = sizeof(array) / sizeof(array[0]);
+	int array[10] = {
+		10, 1, 21, 3, 4, 21, 6, 7, -1, 9
+	};
 
-    printf("Found %d at index: %d\n\n", 3, linear_search(array, size, 3));
-    printf("Found %d at index: %d\n\n", 42, linear_search(array, size, 42));
-    printf("Found %d at index: %d\n", 999, linear_search(array, size, 999));
-    return (EXIT_SUCCESS);
+	printf("Found at index: %d\n", linear_search(array, 10, 21));
+	return (0);
 }
